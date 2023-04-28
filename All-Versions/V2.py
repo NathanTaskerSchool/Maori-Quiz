1aee1e43093bb8da0d03228c5a684ecd89d4d11a
NathanTaskerSchool <124119562+NathanTaskerSchool@users.noreply.github.com>
2023-04-28 14:54:21 +1200

    V2

--- All-Versions/V2.py
+++ All-Versions/V2.py
@@ -1,3 +1,37 @@
+import random
+MAORI_NUMBERS = ["tahi", "rua", "toru", "wha", "rima", "ono", "whitu", "waru", "iwa", "tekau"]
+ENGLISH_NUMBERS = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+MAORI_DAYS = ["ratapu", "rahina", "ratu", "raapa", "rapare", "ramere", "rahoroi"]
+ENGLISH_DAYS = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+MAORI_MONTHS = ["kohi-tatea", "hui-tanguru", "poutu-te-rangi", "paenga-whawha", "haratua", "pipiri"]
+ENGLISH_MONTHS = ["january", "february", "march", "april", "may", "june"]
+
+
+def numbers_quiz():
+    print("test")
+
+
+def days_quiz():
+    print("test")
+
+
+def months_quiz():
+    print("test")
+
+
+def get_questions_amount():
+    
+
+
+def get_quiz(user_input):
+    if user_input == "1":
+        numbers_quiz()
+    elif user_input == "2":
+        days_quiz()
+    elif user_input == "3":
+        months_quiz()
+
+
 def get_quiz_choice():
     print("""Learn the following options in Maori:
 1. Numbers
@@ -13,4 +47,4 @@ def get_quiz_choice():
 
 
 choice = get_quiz_choice()
-print(choice)
+get_quiz(choice)
